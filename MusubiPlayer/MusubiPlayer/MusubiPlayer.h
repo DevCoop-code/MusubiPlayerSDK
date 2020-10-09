c1e61667d045e635f4a0dac6373686e74b7d56c7
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-10-09 09:10:18 +0900

    feat: Make Init Project

---
+++ MusubiPlayer/MusubiPlayer/MusubiPlayer.h
@@ -0,0 +1,19 @@
+//
+//  MusubiPlayer.h
+//  MusubiPlayer
+//
+//  Created by HanGyo Jeong on 2020/10/09.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MusubiPlayer.
+FOUNDATION_EXPORT double MusubiPlayerVersionNumber;
+
+//! Project version string for MusubiPlayer.
+FOUNDATION_EXPORT const unsigned char MusubiPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MusubiPlayer/PublicHeader.h>
+
+
---
+++ MusubiPlayerDemo/MusubiPlayerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusubiPlayerDemo
+//
+//  Created by HanGyo Jeong on 2020/10/09.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
