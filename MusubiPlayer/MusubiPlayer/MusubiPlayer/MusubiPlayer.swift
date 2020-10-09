bae2de45078621ac2734171ef319eb0aecab0729
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-10-09 10:25:08 +0900

    feat: Add MusubiPlayer class

---
+++ MusubiPlayer/MusubiPlayer/MusubiPlayer/MusubiPlayer.swift
@@ -0,0 +1,27 @@
+//
+//  MusubiPlayer.swift
+//  MusubiPlayer
+//
+//  Created by HanGyo Jeong on 2020/10/09.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import MetalKit
+import AVFoundation
+import MobileCoreServices
+
+enum mediaType {
+    case local
+    case hls
+    case dash
+}
+
+class MusubiPlayer: NSObject {
+    var device: MTLDevice?
+    var metalLayer: CAMetalLayer?
+    var pipelineState: MTLRenderPipelineState?
+    var commandQueue: MTLCommandQueue?
+    var avPlayer: AVPlayer?
+}
