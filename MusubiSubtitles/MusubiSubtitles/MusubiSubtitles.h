009f4f4885d358b42c5e8105db8b137734e5326b
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-11-19 10:45:47 +0900

    feat: Add MusubiSubtitles Project

---
+++ MusubiSubtitles/MusubiSubtitles/MusubiSubtitles.h
@@ -0,0 +1,13 @@
+//
+//  MusubiSubtitles.h
+//  MusubiSubtitles
+//
+//  Created by HanGyo Jeong on 2020/11/19.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusubiSubtitles : NSObject
+
+@end
---
+++ MusubiSubtitles/MusubiSubtitles/MusubiSubtitles.m
@@ -0,0 +1,13 @@
+//
+//  MusubiSubtitles.m
+//  MusubiSubtitles
+//
+//  Created by HanGyo Jeong on 2020/11/19.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+#import "MusubiSubtitles.h"
+
+@implementation MusubiSubtitles
+
+@end
